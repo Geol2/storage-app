662867f1b75dd54a65ce06e7ae302e8b6f801a98
geol2 <big9401@gmail.com>
2023-05-29 18:49:03 +0900

    MOD : 메인페이지와 로그인 페이지 구분

--- src/main/java/com/geol/storage/contollers/LoginController.java
+++ src/main/java/com/geol/storage/contollers/LoginController.java
@@ -1,8 +1,11 @@
 package com.geol.storage.contollers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class LoginController {
@@ -10,7 +13,14 @@ public class LoginController {
     public void postLogin(HttpServletRequest request) {
         String userId = request.getParameter("uid");
         String passWd = request.getParameter("pwd");
+    }
 
-        
+    @GetMapping("/login")
+    public ModelAndView getLogin(HttpServletRequest request, Model model) {
+
+        ModelAndView mav = new ModelAndView("main/login");
+        model.addAttribute("data", "hello!");
+
+        return mav;
     }
 }
